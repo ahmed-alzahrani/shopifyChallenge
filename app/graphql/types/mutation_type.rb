e9d8087f152212dde7890608fd5bbc8f87981b71
ahmed-alzahrani <ahmedalzahrani94@gmail.com>
2018-09-16 22:19:53 -0400

    clean slate for monday

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,7 +1,12 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name 'Mutation'
 
+  #product mutations, these also trigger relevant item operations
   field :createProduct, function: Resolvers::CreateProduct.new
   field :updateProduct, function: Resolvers::UpdateProduct.new
   field :deleteProduct, function: Resolvers::DeleteProduct.new
+
+  #user mutations
+  #field :createUser, function: Resolvers::CreateUser.new
+  #field :createOwner, function: Resolvers::CreateOwner.new
 end
